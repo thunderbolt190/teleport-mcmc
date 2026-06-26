11c585efaea186bf15c91c0bb41d9f7897c3abf5
thunderbolt190 <psharish03@gmail.com>
2026-06-26 15:06:10 -0700

    Implement test for weights function
    Add unit test for compute_log_weights function.

--- teleport/utils/weights.py
+++ teleport/utils/weights.py
@@ -1,2 +1,14 @@
-# placeholder
+import pytest
+import numpy as np 
+from teleport.utils.weights import compute_log_weights
 
+def test_weights_function():
+  walkers = np.array([[0.0], [3.0]])
+  log_probs = np.array([-0.5, -2.0])
+  z = np.array([1.0])
+  sigma = 1.0
+  log_w = compute_log_weights(walkers, log_probs, z, sigma)
+
+  actual = [0.01815, 0.07889]
+
+  assert np.allclose(log_w, actual, atol=1e-4)
