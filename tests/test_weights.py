bbdf1d0f8323daa0b5f7aac19694d2e575e5f461
thunderbolt190 <psharish03@gmail.com>
2026-06-26 15:05:44 -0700

    Add unit test for compute_log_weights function
    Added a test for the compute_log_weights function to verify its output against expected values.

--- tests/test_weights.py
+++ tests/test_weights.py
@@ -1,13 +1,14 @@
-from scipy.special import logsumexp
-import numpy as np
+import pytest
+import numpy as np 
+from teleport.utils.weights import compute_log_weights
 
-def compute_log_weights(walkers, log_probs, z, sigma):
-  N = walkers.shape[0]
-  diff = walkers[:, None, :] - walkers[None, :, :]
-  M = -0.5 * np.sum(diff ** 2, axis = -1) / sigma ** 2
-  M[np.arange(N), np.arange(N)] = -np.inf
-  diff_z = walkers - z[None, :]
-  log_q_z = -0.5 * np.sum(diff_z ** 2, axis = -1) / sigma ** 2
-  all_log_q = np.concatenate([log_q_z[:, None], M], axis = 1)
-  log_w = logsumexp(all_log_q, axis = 1) - log_probs
-  return log_w
+def test_weights_function():
+  walkers = np.array([[0.0], [3.0]])
+  log_probs = np.array([-0.5, -2.0])
+  z = np.array([1.0])
+  sigma = 1.0
+  log_w = compute_log_weights(walkers, log_probs, z, sigma)
+
+  actual = [0.01815, 0.07889]
+
+  assert np.allclose(log_w, actual, atol=1e-4)
