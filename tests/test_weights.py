be2ff3e0b846626458e3ba56050b5a66e69038bd
thunderbolt190 <psharish03@gmail.com>
2026-06-29 08:51:11 -0700

    Switch from NumPy to JAX for weight tests

--- tests/test_weights.py
+++ tests/test_weights.py
@@ -1,14 +1,16 @@
 import pytest
-import numpy as np 
+import jax 
+import jax.numpy as jnp
 from teleport.utils.weights import compute_log_weights
+jax.config.update("jax_enable_x64", True)
 
 def test_weights_function():
-  walkers = np.array([[0.0], [3.0]])
-  log_probs = np.array([-0.5, -2.0])
-  z = np.array([1.0])
+  walkers = jnp.array([[0.0], [3.0]])
+  log_probs = jnp.array([-0.5, -2.0])
+  z = jnp.array([1.0])
   sigma = 1.0
   log_w = compute_log_weights(walkers, log_probs, z, sigma)
 
   actual = [0.01815, 0.07889]
 
-  assert np.allclose(log_w, actual, atol=1e-4)
+  assert jnp.allclose(log_w, actual, atol=1e-4)
