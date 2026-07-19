7dbeb3ff6ae28e6e85eea59d2cc491ea3e8a417d
thunderbolt190 <psharish03@gmail.com>
2026-07-19 11:29:04 -0700

    Added JIT compilation to compute_log_weights function

--- teleport/utils/weights.py
+++ teleport/utils/weights.py
@@ -2,6 +2,7 @@ import jax
 import jax.numpy as jnp
 jax.config.update("jax_enable_x64", True)
 
+@jax.jit
 def compute_log_weights(walkers, log_probs, z, sigma):
   N = walkers.shape[0]
   diff = walkers[:, None, :] - walkers[None, :, :]
