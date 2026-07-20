8a56bc2938f57fb9cf79d48162cbfc383732b624
thunderbolt190 <psharish03@gmail.com>
2026-07-19 23:55:10 -0700

    Change variable names to uppercase constants

--- teleport/targets.py
+++ teleport/targets.py
@@ -3,10 +3,10 @@ import jax.numpy as jnp
 def log_prob_doublewell(x):
     return jnp.sum(-4.0 * (x**4 - x**2))
 
-gaussian2d_mean = jnp.array([2.0, -1.0])
-gaussian2d_cov = jnp.array([[1.0, 0.8], [0.8, 1.0]])
-gaussian2d_covinv = jnp.linalg.inv(gaussian2d_cov)
+GAUSSIAN2D_MEAN = jnp.array([2.0, -1.0])
+GAUSSIAN2D_COV = jnp.array([[1.0, 0.8], [0.8, 1.0]])
+GAUSSIAN2D_COVINV = jnp.linalg.inv(GAUSSIAN2D_COV)
 
-def log_prob_gaussian2d(x, mean=gaussian2d_mean, covinv=gaussian2d_covinv):
+def log_prob_gaussian2d(x, mean = GAUSSIAN2D_MEAN, covinv = GAUSSIAN2D_COVINV):
     diff = x - mean
     return -0.5 * diff.T @ covinv @ diff
